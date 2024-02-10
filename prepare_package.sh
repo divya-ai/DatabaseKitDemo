c7f046a1ebfcdc92424fefa25f42e8682b91621a
Divya Prakash <divya@Divyas-MacBook-Air.local>
2024-02-11 01:39:42 +0530

    New 0.0.1 version DatabaseKit

--- prepare_package.sh
+++ prepare_package.sh
@@ -36,7 +36,7 @@ git push
 
 # 6. Pusblish a new release with the same version of the repository A, and attach XCFramework in the Release metadata
 echo "Releasing the new version"
-gh release create "$NEW_VERSION" --generate-notes "./$FRAMEWORK_NAME.xcframework.zip"
+git release create "$NEW_VERSION" --generate-notes "./$FRAMEWORK_NAME.xcframework.zip"
 
 # 7. Remove zip of XCFramework
 echo "delete downloaded zip file"
